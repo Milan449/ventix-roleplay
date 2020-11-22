2cb05b9af84457a8bd68af154396e4584bf77e5b
Milan449 <72096034+Milan449@users.noreply.github.com>
2020-11-22 16:39:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,7 @@ const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('online en klaar voor gebruik');
-});
 
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
-	if (command === 'ping') {
-		message.channel.send('Pong.');
-	} else if (command === 'beep') {
-		message.channel.send('Boop.');
-	}
-	// other commands...
 });
 
 bot.login(procces.env.token);
